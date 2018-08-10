452324c1b422ab3e21482d0785c499df144d983c
chenhuayang <chenhuayang@dooffe.com>
2018-08-10 09:28:11 +0800

    Revert "first"
    This reverts commit 246bf1db6ae80a2cea0efabff221bbfd7d1f5887.

--- hello.js
+++
@@ -1,8 +0,0 @@
-import React,{Component} from 'react';
-import {StyleSheet,Text, View} from 'react-native';
-
-export default class Hello extends Component{
-    render(){
-        return <Text style={{fontSize:20, backgroundColor:'red'}}>Hello</Text>
-    }
-}
